28dbdd4c0d796681a5848ce5b4cf47bbd82607dd
marcelocorreia <marcelo.correia@starvisitor.com>
2017-04-25 02:28:57 +1000

    lazy dev

--- cicd/create_some_files.sh
+++ cicd/create_some_files.sh
@@ -5,3 +5,6 @@ echo "file1" > some-files/file1
 echo "file2" > some-files/file2
 echo "file3" > some-files/file3
 echo "file4" > some-files/file4
+echo "file5" > some-files/file5
+echo "file6" > some-files/file6
+echo "file7" > some-files/file7
