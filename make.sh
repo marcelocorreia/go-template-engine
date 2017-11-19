2b03f70c571ae43db3f76e1c62a660f25bca2f13
Marcelo Correia <marcelo.correia@starvisitor.com>
2017-11-19 15:39:33 +1100

    wip

--- make.sh
+++ make.sh
@@ -10,8 +10,8 @@ if [[ -f resource_version/version ]];then
     VERSION=$(cat resource_version/version)
 fi
 
-#echo VERSION=$VERSION
-#
-#dir=$(dirname $0)
+echo VERSION=$VERSION
 
-#make -C ${dir} ${MAKE_TARGETS} VERSION=$VERSION
+dir=$(dirname $0)
+
+make -C ${dir} ${MAKE_TARGETS} VERSION=$VERSION
