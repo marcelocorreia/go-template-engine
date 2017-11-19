8cac321090078ed23c4a0d3c260d3c0e06720fbb
Marcelo Correia <marcelo.correia@starvisitor.com>
2017-11-19 13:58:29 +1100

    wip

--- ci.mk
+++ ci.mk
@@ -1,23 +1,23 @@
-_build:
-	@$(call ci_make,lint build GOOS=linux)
-
-_test:
-	@$(call ci_make,lint test GOOS=linux)
-
-_release:
-	@$(call ci_make,release)
-
-_prepare:
-	@echo $(GOPATH) - $(shell pwd)
-	@mkdir -p /go/src/$(NAMESPACE)/$(APP_NAME)/dist
-	@cp -R * /go/src/$(NAMESPACE)/$(APP_NAME)/
-	@$(call ci_make,deps)
-
-define ci_make
-	echo ""
-	echo "*** $1::Begin ***"
-	cd $(GOPATH)/src/$(NAMESPACE)/$(APP_NAME) && \
-    		make $1
-	echo "*** $1::End ***"
-	echo ""
-endef
+#_build:
+#	@$(call ci_make,lint build GOOS=linux)
+#
+#_test:
+#	@$(call ci_make,lint test GOOS=linux)
+#
+#_release:
+#	@$(call ci_make,release)
+#
+#_prepare:
+#	@echo $(GOPATH) - $(shell pwd)
+#	@mkdir -p /go/src/$(NAMESPACE)/$(APP_NAME)/dist
+#	@cp -R * /go/src/$(NAMESPACE)/$(APP_NAME)/
+#	@$(call ci_make,deps)
+#
+#define ci_make
+#	echo ""
+#	echo "*** $1::Begin ***"
+#	cd $(GOPATH)/src/$(NAMESPACE)/$(APP_NAME) && \
+#    		make $1
+#	echo "*** $1::End ***"
+#	echo ""
+#endef
