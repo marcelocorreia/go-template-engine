6397c0467e7cb8788483b4d4b50f4bd2975c6830
Marcelo Correia <marcelo.correia@starvisitor.com>
2017-11-19 14:48:23 +1100

    wip

--- make.sh
+++ make.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+VERSION=dev
+
+if [[ -f ../resource-version/version ]];then
+    VERSION=$(cat ../resource-version/version)
+    echo VERSION=$VERSION
+fi
+
 dir=$(dirname $0)
 
 make -C ${dir} ${MAKE_TARGETS}
