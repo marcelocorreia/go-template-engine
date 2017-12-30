5e619babf08fbe433e379df020ecc295a40ab6af
Marcelo Correia <marcelo.correia@starvisitor.com>
2017-12-31 04:05:27 +1100

    updating pipeline

--- make.sh
+++ make.sh
@@ -3,14 +3,14 @@
 dir=$(dirname $0)
 
 VERSION=dev
-HOMEBREW_REPO_PATH=$(pwd)/${dir}/../homebrew-repo
+#HOMEBREW_REPO_PATH=$(pwd)/${dir}/../homebrew-repo
 
 if [[ -f app-resource-version/version ]];then
     VERSION=$(cat app-resource-version/version)
 fi
 
 echo VERSION=${VERSION}
-echo HOMEBREW_REPO_PATH=${HOMEBREW_REPO_PATH}
+#echo HOMEBREW_REPO_PATH=${HOMEBREW_REPO_PATH}
 
 
 make -C ${dir} ${MAKE_TARGETS} VERSION=$VERSION
