436b20b1f459b9f2494c6da11c647416d4b2eac8
Marcelo Correia <marcelo.correia@starvisitor.com>
2018-01-06 11:37:48 +1100

    updating pipeline

--- template-engine/template-engine.go
+++ template-engine/template-engine.go
@@ -12,7 +12,7 @@ import (
 	"text/template"
 	"bufio"
 	"errors"
-	"github.com/marcelocorreia/go-template-engine/utils"
+	"github.com/marcelocorreia/go-template-engine/aux"
 )
 
 var DELIMS = []string{"{{", "}}"}
@@ -132,7 +132,7 @@ func (gte TemplateEngine) VariablesFileMerge(varsFile []string, extra_vars map[s
 		return "", err
 	}
 	os.Remove(tmpFile.Name())
-	utils.CopyFile(cleanFile, cleanFile+".yml")
+	aux.CopyFile(cleanFile, cleanFile+".yml")
 
 	return cleanFile + ".yml", nil
 }
@@ -171,7 +171,7 @@ func (gte TemplateEngine) ProcessDirectory(sourceDir string, targetDir string, p
 		targetFile := fmt.Sprintf("%s/%s", targetDir, f)
 		var body string
 		baseName := filepath.Base(sourceFile)
-		if utils.StringInSlice(baseName, fileIgnores) {
+		if aux.StringInSlice(baseName, fileIgnores) {
 			c, err := ioutil.ReadFile(sourceFile)
 			if err != nil {
 				return err
@@ -200,7 +200,7 @@ func (gte TemplateEngine) GetFileList(dir string, fullPath bool, dirExclusions [
 		return nil, err
 	}
 	for _, f := range files {
-		if !utils.StringInSlice(f.Name(), dirExclusions) && !utils.StringInSlice(f.Name(), fileExclusions) {
+		if !aux.StringInSlice(f.Name(), dirExclusions) && !aux.StringInSlice(f.Name(), fileExclusions) {
 			if info, err := os.Stat(dir + "/" + f.Name()); err == nil && info.IsDir() {
 				gte.getTempList(dir+"/"+f.Name(), fileList)
 			} else {
@@ -238,15 +238,15 @@ func (gte TemplateEngine) PrepareOutputDirectory(sourceDir string, targetDir str
 		return errors.New("output must be provided when source is a directory")
 	}
 
-	utils.CreateNewDirectoryIfNil(targetDir)
+	aux.CreateNewDirectoryIfNil(targetDir)
 	files, err := ioutil.ReadDir(sourceDir)
 	if err != nil {
 		return err
 	}
 	for _, d := range files {
-		if !utils.StringInSlice(d.Name(), exclusions) {
+		if !aux.StringInSlice(d.Name(), exclusions) {
 			if info, err := os.Stat(sourceDir + "/" + d.Name()); err == nil && info.IsDir() {
-				utils.CreateNewDirectoryIfNil(targetDir + "/" + d.Name())
+				aux.CreateNewDirectoryIfNil(targetDir + "/" + d.Name())
 			}
 		}
 	}
