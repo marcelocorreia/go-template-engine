9f2afd3157ecd8969b02e7760c19d17db80723bc
Marcelo Correia <marcelo.correia@starvisitor.com>
2019-04-16 23:01:49 +1000

    Releasing 3.1.4

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/marcelocorreia/go-template-engine/template-engine"
+	"github.com/marcelocorreia/go-template-engine/templateengine"
 	"gopkg.in/alecthomas/kingpin.v2"
 	"io/ioutil"
 	"os"
